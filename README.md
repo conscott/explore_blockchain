3106439cff69c8068e3c7bafca567196487ea10c
Conor Scott <conor.r.scott.88@gmail.com>
2018-03-27 17:56:52 +0400

    Fix self-spend condition

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #### Description
 WIP tool to inspect blockchain transactions and look for certain sequences. Requires a connection to a full node with a full transcation index (`-txindex=1`)
+
+Example: Find transactions with output greater than 1000 BTC, having input with previous microtransaction of > 0.01
+The chain would indicate a "proof of ownership tx" before a large OTC trade
+```
+./inspect --gt=1000 --veramt=0.001
+```
--- inspect.py
+++ inspect.py
@@ -22,10 +22,11 @@ parser = argparse.ArgumentParser(add_help=True,
                                  epilog='''Help text and arguments for individual test script:''',
                                  formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument('--gt', type=int, default=5000, help='List txs with output greater than this value (in BTC)')
-parser.add_argument('--veramt', type=float, default=0.1, help='Assume verify transaction is less than value (in BTC)')
+parser.add_argument('--gt', type=int, default=1000, help='List txs with output greater than this value (in BTC)')
+parser.add_argument('--veramt', type=float, default=0.01, help='Assume verify transaction is less than value (in BTC)')
 parser.add_argument('--maxblocks', type=int, default=1000, help='Maximum number of blocks to scan')
 parser.add_argument('--maxdepth', type=int, default=1, help='Maximum depth of back-scan')
+parser.add_argument('--tx', type=str, help='Try a certain tx')
 args, unknown_args = parser.parse_known_args()
 
 if unknown_args:
@@ -44,36 +45,48 @@ def follow_link(tx):
 
 # If found a tx with a large output, see if inputs are linked to previous tx
 # with tiny output, make function recursive to explore depth
-def back_scan(txinfo, verify_tx_amount, depth):
-    print("Back-scanning %s Inputs" % len(txinfo['vin']))
+# TODO - scan back txs or based on address
+def back_scan(txinfo, deposit_address, big_amount, verify_tx_amount, depth):
+    print("Back-scanning %s Inputs for deposit to %s" % (len(txinfo['vin']), deposit_address))
     for vin in txinfo['vin']:
         spent_txid = vin['txid']
-        vout_spent = vin['vout']
         spent_tx = rpc.getrawtransaction(spent_txid, 'true')
+
+        # First check it's not sending to itself in the big amount
+        for vout in spent_tx['vout']:
+            if vout['value'] >= big_amount and vout['scriptPubKey']['addresses'][0] == deposit_address:
+                print("Large transaction is just spending to itself, not interesting...")
+                return False
+
         for vout in spent_tx['vout']:
             if vout['value'] <= verify_tx_amount:
-                print("Microtransaction deposited to %s" % vout['scriptPubKey']['addresses'])
+                print("Micro transaction in tx %s with value of %s" % (spent_txid, vout['value']))
+                print("deposited to %s" % vout['scriptPubKey']['addresses'])
                 return True
     return False
 
 
-# If tx has been seen in backscan before, can skip
-seen_txs = set()
-current_block = rpc.getblockcount()
-for block_height in range(current_block, current_block-args.maxblocks, -1):
-    block_hash = rpc.getblockhash(block_height)
-    txs = rpc.getblock(block_hash)['tx']
-    print("Inspecting block %s with %s txs" % (block_hash, len(txs)))
-    # Scan each tx in recent blocks
-    for tx in txs:
-        print("Inspect %s" % tx)
-        txinfo = rpc.getrawtransaction(tx, 'true')
-        # Scan outputs of each transaction
-        for vout in txinfo['vout']:
-            if vout['value'] > args.gt:
-                print("Inspecting tx %s with output of %s BTC" % (tx, vout['value']))
-                seen_txs.add(tx)
-                if back_scan(tx, args.veramt, args.maxdepth):
-                    print("Tx %s is a winner!" % tx)
-                    follow_link(tx)
-                    sys.exit(0)
+def inspect_tx(tx):
+    txinfo = rpc.getrawtransaction(tx, 'true')
+    # Scan outputs of each transaction
+    for vout in txinfo['vout']:
+        if vout['value'] >= args.gt:
+            print("Inspecting tx %s with output of %s BTC" % (tx, vout['value']))
+            deposit_address = vout['scriptPubKey']['addresses'][0]
+            if back_scan(txinfo, deposit_address, vout['value'], args.veramt, args.maxdepth):
+                print("Tx %s is a winner!" % tx)
+                follow_link(tx)
+                sys.exit(0)
+
+
+if not args.tx:
+    current_block = rpc.getblockcount()
+    for block_height in range(current_block, current_block-args.maxblocks, -1):
+        block_hash = rpc.getblockhash(block_height)
+        txs = rpc.getblock(block_hash)['tx']
+        print("Inspecting block %s with %s txs" % (block_hash, len(txs)))
+        # Scan each tx in recent blocks
+        for tx in txs:
+            inspect_tx(tx)
+else:
+    inspect_tx(args.tx)
